19fb8b354a18313a6a7f3451c8cd9d1fc6835032
dewmal <dewmal@syigen.com>
2025-09-17 21:22:09 +0530

    Add UI to get search results

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 # api.py
 from typing import List, Optional
-from fastapi import FastAPI, Query, HTTPException
+from fastapi import FastAPI, Query, HTTPException,Request
 from pydantic import BaseModel, HttpUrl
 from starlette.concurrency import run_in_threadpool
-
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 # ⬇️ import your function (update the module path to where you keep it)
 from app.vehicle_finder_ikman import get_vehicle_details  # e.g., put your code in scraper.py
 
 app = FastAPI(title="Ikman Vehicle Scraper API", version="1.0.0")
-
+# Tell FastAPI where your templates folder is
+templates = Jinja2Templates(directory="templates")
 
 class AdItem(BaseModel):
     title: str
@@ -19,6 +21,11 @@ class AdItem(BaseModel):
     price: str
     updated: str
 
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    # request is required by Jinja2Templates
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/vehicles", response_model=List[AdItem])
 async def vehicles(q: str = Query(..., min_length=1, description="Search term, e.g. 'Alto'")):
